d13a4e63db22315a050e90ecc58fd0be813cd241
Arthur Bréant <arthur@thinkr.fr>
2024-07-14 14:20:46 +0200

    chore: custom app custom variables

--- signature/app.py
+++ signature/app.py
@@ -1,7 +1,14 @@
 from shiny import App, Inputs, Outputs, Session, ui
 from modules import mod_navbar
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+
+current_dir = Path(__file__).parent
+
+env = Environment(loader=FileSystemLoader(current_dir))
 
 app_ui = ui.page_fixed(
+    ui.include_css(current_dir / "css" / "signature.css"),
     mod_navbar.navbar_ui("nav_signature"),
 )
 
